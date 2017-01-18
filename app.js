8e7d17a90344a618409b0a86383aef95b0b73896
yogicoder <sukyee.gong@gmail.com>
2017-01-18 10:32:03 +0800

    Test

---
+++ app.js
@@ -0,0 +1,19 @@
+//use javascript to hide and show red box
+function js1(){
+  document.getElementById("box").style.display = "none";
+}
+
+function js2(){
+  document.getElementById("box").style.display = "block";
+}
+
+
+//use jquery to hide and show red box
+function jquery1(){
+  $("#box").hide();
+}
+
+function jquery1(){
+  $("#box").show();
+}
+
